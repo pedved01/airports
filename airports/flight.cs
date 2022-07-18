9b3688e6f2dd3bb5945bf2e7e2760f9eeb9f3dc7
pedved <somichandriicz@gmail.com>
2022-07-18 07:28:05 +0200

    with library

---
+++ airports/flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace airports
+{
+    public enum Flight_Type
+    {
+        arrival,
+        depature
+    }
+    internal class Flight
+    {
+        public DateTime date_time { get; set; }
+        public int ID { set; get; }
+        public char terminal { set; get; }
+        public string aviacompany { set; get; }
+        public int gate { set; get; }
+        public Flight_Type flight_Type { set; get; }
+    }
+}
+
